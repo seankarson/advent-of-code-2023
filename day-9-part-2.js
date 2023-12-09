69492d6faf0cbf68adcf81c4b3702e595cc72aec
Sean Karson <sean@golfscope.com>
2023-12-09 00:16:43 -0500

    Add day 9 solutions

---
+++ day-9-part-2.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const fileContents = fs.readFileSync('./day-9-input.txt', 'utf-8').split('\n');
+
+const getReducedLine = (numbers) => {
+  let currentNumbers = numbers;
+  let result = [currentNumbers];
+
+  while (currentNumbers.some(val => val !== 0)) {
+    let newCurrentNumbers = [];
+
+    for (let i = 1; i < currentNumbers.length; i++) {
+      newCurrentNumbers.push(currentNumbers[i] - currentNumbers[i - 1]);
+    }
+
+    result.push(newCurrentNumbers);
+    currentNumbers = newCurrentNumbers;
+  }
+  return result;
+}
+
+let sum = 0;
+for (const line of fileContents) {
+  const reducedLine = getReducedLine(line.split(' ').map(val => parseInt(val)));
+
+  let finalValue = 0;
+  for (let i = reducedLine.length - 1; i > 0; i--) {
+    const valueToAdd = reducedLine[i - 1][0] - reducedLine[i][0];
+    finalValue = valueToAdd;
+    reducedLine[i - 1] = [valueToAdd, ...reducedLine[i - 1]]
+  }
+  sum += finalValue;
+}
+
+console.log(sum);
